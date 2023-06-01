8d2a6fe9d8cf351c7429747df1be3dd96138c160
Bozorgmehr Zia <bozorgmehr1381@gmail.com>
2023-06-01 10:34:18 +0330

    first

--- predictor.v
+++ predictor.v
@@ -1,5 +1,22 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
-
-// Your code
-
+  //reg [3:0] history_table [15:0];
+  reg [1:0] counter;
+  //reg [3:0] history_index;
+  //reg last_taken;
+  initial begin
+    counter = 2'b0;
+  end
+  
+  always @(posedge clk) begin
+    if (request) begin
+      prediction = counter > 1;
+    end
+      if (result) begin
+        if (taken) begin
+          counter = counter + 1;
+        end else begin
+          counter = counter - 1;
+        end
+      end
+  end
 endmodule
